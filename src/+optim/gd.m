94d5356265953dc328ef8498b49a2c3eb4aa23f0
zjw <charliezijianwang@gmail.com>
2021-06-29 11:19:59 -0500

    gd works for quadratic

--- src/+optim/gd.m
+++ src/+optim/gd.m
@@ -1,5 +1,5 @@
 function [opt, log] = gd(fun, init, cparams)
-    %todo: 
+    %todo: doc
     %convergence criteria: grad 2-norm < cparams.eps
 
     %read gradient descent parameters
@@ -17,11 +17,13 @@ function [opt, log] = gd(fun, init, cparams)
     gradient_descent_converged = false;
 
     for i = 1:maxiter
-        if gradient_descent_converged:
+        if gradient_descent_converged
+            fprintf('gradient descent converged after %d steps\n', ...
+            i);
             break;
         end
 
-        grad = gd_grad(@fun, opt, hspace);
+        grad = optim.gd_grad(fun, opt, hspace);
         grad_norm = norm(grad);
         loss = fun(opt);
         gradient_descent_converged = (grad_norm < eps);
@@ -51,13 +53,15 @@ function [opt, log] = gd(fun, init, cparams)
                     if better_loss < left
                         step = better_step;
                     end
-                elif step < line_search_eps
+                elseif step < line_search_eps
                     line_search_converged = true;
                 else
                     step = step * line_search_beta;
                 end
             end
-
+        
+            %update weight
+            opt = opt - step * grad;
 
         %record
         loss_arr(i) = loss;
@@ -67,12 +71,30 @@ function [opt, log] = gd(fun, init, cparams)
     end
 
     if not(gradient_descent_converged)
-        fprintf('gradient descent not converged after %d steps', ...
+        fprintf('gradient descent not converged after %d steps\n', ...
             maxiter);
     end
     
-    log = { 'loss',   loss_arr, ...
+    log = struct( ...
+            'loss',   loss_arr, ...
             'step',   step_arr, ...
             'grad',   grad_arr, ...
-            'weight', weight_arr};
+            'weight', weight_arr);
 end
+
+
+
+% %function that computes gradient
+% function grad = gd_grad(fun, xval, hspace)
+%     %evaluates gradient using finite difference
+%     num_params = length(xval);
+%     grad = zeros(num_params);
+%     for i = 1:num_params
+%         xval(i) = xval(i) + hspace;
+%         disp(xval)
+%         fun_val_forward = fun(xval);
+%         xval(i) = xval(i) - 2*hspace;
+%         fun_val_backward = fun(xval);
+%         grad(i) = (fun_val_forward + fun_val_backward)/(2*hspace);
+%     end
+% end
