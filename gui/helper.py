82eade2cd008104ce77d0b15c94e7ebfbca5a2ae
zjw <charliezijianwang@gmail.com>
2021-07-12 14:01:21 -0500

    move point wip

--- gui/helper.py
+++ gui/helper.py
@@ -26,3 +26,9 @@ def delete_temp_file(file_id):
 def init_poly(num_verts):
     vert_angles = np.arange(0, 2*np.pi, 2*np.pi/num_verts)[:num_verts]
     return [list(np.cos(vert_angles)),list(np.sin(vert_angles))]
+
+def real2gui_linear(x,y,a,bx,by):
+    return [(x+bx)*a, (y+by)*a]
+
+def gui2real_linear(x,y,a,bx,by):
+    return [x/a-bx, y/a-by]
