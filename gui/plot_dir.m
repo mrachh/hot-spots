83473868b9f8ad1b8b2d3c5ca0be031dac2f109b
zjw <charliezijianwang@gmail.com>
2021-07-16 16:35:32 -0500

    wip

--- gui/plot_dir.m
+++ gui/plot_dir.m
@@ -1,45 +1,56 @@
-function [utot] = plot_new(chnk_plot_axis, chnkr, k, direction, xlimit, ylimit)
+function [nothing] = plot_new(chnk_plot_axis, chnkr, sol, zk)
+    disp(sol(2))
     %%%
-    5 text inputs
-    zk, xlim, ylim
-    zk slider; .1 - 40
-    two more imput: F, boundary data
+    % 5 text inputs
+    % zk, xlim, ylim
+    % zk slider; .1 - 40
+    % two more imput: F, boundary data
 
-    when chunkie update (include slide): 
-        do create F in backgorund
-        do compute boundary
+    % when chunkie update (include slide): 
+    %     do create F in backgorund
+    %     do compute boundary
 
-    after xlim, ylim,
-        find exterior points
+    % after xlim, ylim,
+    %     find exterior points
     %%%
+
     addpath('../src');
-    chnk_plot_handle = [];
-    kvec = k .* [cos(direction); sin(direction)];
-    zk = k
+    % chnk_plot_handle = [];
+    % kvec = zk .* [cos(direction); sin(direction)];
     nplot = 200;
 
-    %%%%%%%%%%%%%%%%%%
-    % solve and visualize the solution
+    % %%%%%%%%%%%%%%%%%%
+    % % solve and visualize the solution
 
-    % build CFIE
+    % % build CFIE
 
     fkern = @(s,t) chnk.helm2d.kern(zk,s,t,'c',1);
-    opdims(1) = 1; opdims(2) = 1;
-    opts = [];
-    start = tic; sysmat = chunkermat(chnkr,fkern,opts);
-    t1 = toc(start);
+    % opdims(1) = 1; opdims(2) = 1;
+    % opts = [];
+    % start = tic; sysmat = chunkermat(chnkr,fkern,opts);
+    % t1 = toc(start);
 
-    fprintf('%5.2e s : time to assemble matrix\n',t1)
+    % fprintf('%5.2e s : time to assemble matrix\n',t1)
 
-    sys = 0.5*eye(chnkr.k*chnkr.nch) + sysmat;
+    % sys = 0.5*eye(chnkr.k*chnkr.nch) + sysmat;
 
-    rhs = -planewave(kvec(:),chnkr.r(:,:)); rhs = rhs(:);
-    start = tic; sol = gmres(sys,rhs,[],1e-13,100); t1 = toc(start);
+    % rhs = -planewave(kvec(:),chnkr.r(:,:)); rhs = rhs(:);
+    % start = tic; sol = sys\rhs; t1 = toc(start);
 
-    fprintf('%5.2e s : time for dense gmres\n',t1)
+    % fprintf('%5.2e s : time for dense gmres\n',t1)
 
-    xtarg = linspace(xlimit(1),xlimit(2),nplot); 
-    ytarg = linspace(ylimit(1),ylimit(2),nplot);
+    % %%%%%%%%%%%%%%%%%%%%%%
+    % % End of solve
+
+    rmin = min(chnkr); rmax = max(chnkr);
+    xl = rmax(1)-rmin(1);
+    yl = rmax(2)-rmin(2);
+    xlo = rmin(1)-xl;
+    xhi = rmax(1)+xl;
+    ylo = rmin(2)-yl;
+    yhi = rmax(2)+yl;
+    xtarg = linspace(xlo,xhi,nplot); 
+    ytarg = linspace(ylo,yhi,nplot);
     [xxtarg,yytarg] = meshgrid(xtarg,ytarg);
     targets = zeros(2,length(xxtarg(:)));
     targets(1,:) = xxtarg(:); targets(2,:) = yytarg(:);
@@ -71,11 +82,12 @@ function [utot] = plot_new(chnk_plot_axis, chnkr, k, direction, xlimit, ylimit)
     zztarg = nan(size(xxtarg));
     zztarg(out) = utot;
 
-    % axes(chnk_plot_axis)
     h=pcolor(chnk_plot_axis ,xxtarg,yytarg,imag(zztarg));
-    set(h,'EdgeColor','none')
-    colormap(chnk_plot_axis, redblue)
-    % set(gcf,'Visible', 'off');
-    % caxis([-maxu,maxu])
-
+    set(h,'EdgeColor','none');
+    colormap(chnk_plot_axis, redblue);
+    caxis([-maxu,maxu]);
+    % quick fix
+    set(gcf,'Visible', 'off');
+    % set axis 
+    axis(chnk_plot_axis, [xlo xhi ylo yhi]);
 end
