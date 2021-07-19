3c1677bcf2581d7ce2a03ec908fd7b6e2c383f14
zjw <charliezijianwang@gmail.com>
2021-07-19 15:12:03 -0500

    preproc verts done

--- gui_final/preproc_verts.m
+++ gui_final/preproc_verts.m
@@ -1,4 +1,7 @@
 function [nice_verts] = preproc_verts(verts)
-    nice_verts = verts
+    % preprocess the vertices
+    % vertices are of shape 2*n
+
+    nice_verts = polyshape(verts').Vertices';
 
 end
--- gui_final/test_preproc_verts.m
+++ gui_final/test_preproc_verts.m
@@ -1,4 +1,6 @@
 test_pgon = nsidedpoly(5);
 verts = nsidedpoly(5).Vertices';
 flipped = flipud(verts')';
-fprintf('err: %5.2e\n', norm(preproc_verts(flipped) - verts))
+verts
+flippedflippedverts = preproc_verts(flipped)
+disp('works up to some permutation')
