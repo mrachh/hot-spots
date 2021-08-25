ee9178b61ab76ae5454683050c27be8dff039ebf
zjw <charliezijianwang@gmail.com>
2021-08-25 08:08:39 -0500

    wip

--- shape_optimization_summarize/test.m
+++ shape_optimization_summarize/test.m
@@ -1,10 +1,11 @@
 clear;clc;addpath('../src');
 
-res = load('gd_log2.mat');
+res = load('gd_sym13.mat');
 num_steps = 100;
 
+
 for i = 1:num_steps
-    chnkr = polysymeven_chnk(res.weight(i,:));
+    chnkr = chnk_polyeven(res.weight(i,:));
     f = figure(1);
     clf;
     plot(chnkr);
@@ -12,5 +13,5 @@ for i = 1:num_steps
     ylim([-1 3]);
     title(sprintf('iter: %d, loss: %.6f',i,res.loss(i)));
     f.Position = [100 100 600 600];
-    saveas(f,sprintf('temp/3/%d.png',i));
+    saveas(f,sprintf('temp/gd_sym13/%d.png',i));
 end
