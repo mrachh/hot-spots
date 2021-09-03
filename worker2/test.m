1852c4a9c5dd507a386d6d22b224257dd7fc3b28
zjw <charliezijianwang@gmail.com>
2021-09-03 14:17:13 -0500

    doc

--- worker2/test.m
+++ worker2/test.m
@@ -1,36 +1,11 @@
 clear;clc;addpath('../src');
 
-diary('test_optimization_diary.txt');
-
 num_verts = 20;
 num_params = num_verts;
 
-
-
-init_weight = ones(1,num_verts)/sqrt(pi);
-
-% Last time:
-% [0.408066774854523 0.429040642463146 1.18253139126812 0.65423897111139 0.443338318926292 0.440662202602962 0.639294305380822 1.15659966820638]
-
-% init_weight = [0.408066774854523 0.429040642463146 1.18253139126812 0.65423897111139 0.443338318926292 0.440662202602962 0.839294305380822 1.15659966820638];
-
-plot(chnk_poly(init_weight))
-error('nothing')
-
-
-% valid_verts = ones(1, num_params);
-% plot(chnk_polyeven(init_weight, valid_verts))
-% error('nothing')
-% Gradient descent parameter
-gd_params = struct( ...
-    'maxiter',          100, ...
-    'report',           1, ...
-    'eps',              1e-6,   ...
-    'hspace',           1e-3, ...
-    'line_search_eps',  1e-4, ...
-    'line_search_beta', 0.5, ...
-    'savefn',           'gd.mat' ...
-)
+weight = ones(1, num_params)/sqrt(pi/2);
+grad_arr = nan(100, num_params);
+h_arr = nan(1, 100);
 
 loss_params = struct(...
     'default_chebabs',      [1 10], ...
@@ -39,14 +14,25 @@ loss_params = struct(...
     'unorm_ord',                  '2' ...
 )
 
-% Set loss function
-fun = @loss;
 
-fprintf('Polygon polygon with %d vertices\n', num_verts);
-[opt, gd_log] = gd(@loss, init_weight, gd_params, loss_params);
-fprintf('optimal weight: %5.2e \n', opt);
-fprintf('optimal value: %5.2e \n', fun(opt));
-clear fun
+[~, chebab] = loss(weight,loss_params);
 
+% Convergence test
 
-diary off;
+num_grads = 0;
+for logh = -1.5:-0.1:-5
+    num_grads = num_grads + 1
+    hspace = 10^(logh)
+    h_arr(num_grads) = hspace;
+    grad = zeros(1, num_params);
+    for param_idx = 1:num_params
+        direction = zeros(1, num_params);
+        direction(param_idx) = hspace;
+        left = loss(weight - direction, loss_params, chebab);
+        right = loss(weight + direction, loss_params, chebab);
+        grad(param_idx) = (right - left) / (2 * hspace);
+    end
+    grad_arr(num_grads,:) = grad;
+end
+
+grad_arr = grad_arr(1:num_grads,:);
