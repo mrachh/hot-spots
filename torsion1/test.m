f5b2ff328d62bed0a86e2489ff548d80f90bef5e
zjw <charliezijianwang@gmail.com>
2021-09-21 21:37:47 -0500

    wip

--- torsion1/test.m
+++ torsion1/test.m
@@ -1,2 +1,11 @@
-clear;clc;addpath('../src')
+clear;clc;addpath('../src');
+
+
+num_params = 10;
+rs = ones(1, num_params);
+chnkr = chnk_poly(rs);
+
+% kern = (x-x') dot n(x') / (x-x')^2
+% system: (-pi*id + kern_mat)sigma = x_2^2/2
+
 
